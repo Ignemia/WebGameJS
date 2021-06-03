c730245925956e78afd232ba292a36a264119d49
opalecky <55623577+opalecky@users.noreply.github.com>
2021-06-03 09:04:36 +0200

    Description for constants

--- src/scripts/components/physics/physics.comp.ts
+++ src/scripts/components/physics/physics.comp.ts
@@ -6,7 +6,12 @@
 //          Final: Explosions
 
 export const CONSTANTS = {
-    c: 299792458, // meters per second
-    g: 9.81, // m/s^2
-    G: 6.67430*(1/Math.pow(10, 11)),
+    // speed of light = 299 792 458 [ m / s ]
+    c: 299792458,
+
+    // average gravitational acceleration in earths gravitational field = 9.81 [ m / s^2^ ]
+    g: 9.81,
+
+    // Einsteins' gravitational constant = 6.6743015 × 10^-11^ [ m^3^ / ( kg * s^2^ ) ]
+    G: 6.6743015*(1/Math.pow(10, 11)),
 }
